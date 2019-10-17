0138ebdad4e6c54bc5244dd07f8209cc3399fd8c
Taylor-Brenden <btay46@gmail.com>
2019-10-17 15:57:16 -0600

    Update main.js

--- karaoke/js/main.js
+++ karaoke/js/main.js
@@ -1,13 +1,23 @@
 var btn = document.getElementById("btn");
 var musicContainer = document.getElementById("music-info");
 
-var pull = new XMLHttpRequest();
-pull.open('GET', '../api/music_list.json');
+btn.addEventListener("click", function() {
+    var pull = new XMLHttpRequest();
+    
+    pull.open('GET', '../api/music_list.json');
+    
+    pull.onload = function(){
+        if (pull.status >= 200 && pull.status < 400) {
+        var ourData = JSON.parse(pull.responseText);
+        renderHTML(ourData);
+        } else {
+            console.log("We connected to the server, but it returned an error.");
+        }
+    }
 
-pull.onload = function(){
-    var ourData = JSON.parse(pull.responseText);
-    renderHTML(ourData);
-}
+    pull.send();
+
+});
 
 //This is a test that outputs first 5 songs in database from the API
 function renderHTML(data){
@@ -17,5 +27,6 @@ function renderHTML(data){
         htmlString += "<p>" + data[i].song + " is a song by " + data[i].artist;
         htmlString += "</p>";
     }
+    
     musicContainer.insertAdjacentHTML('beforeend', htmlString);
 }
