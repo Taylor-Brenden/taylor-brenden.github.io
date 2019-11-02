2abb4b5e8c4647d52f32de36d895b9f2b435a61f
Taylor-Brenden <btay46@gmail.com>
2019-11-02 13:27:41 -0600

    Create main.js

---
+++ CSSxJS/main.js
@@ -0,0 +1,3 @@
+//Select steps two through four and then add the class "hide" to them.
+var futureStep = document.querySelectorAll("#stepTwo, #stepThree, #stepFour");
+futureStep.classList.add("hide");
