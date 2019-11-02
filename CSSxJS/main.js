b5dbae3c3e381430146aba358a021695bcb3f287
Taylor-Brenden <btay46@gmail.com>
2019-11-02 15:09:17 -0600

    Update main.js

--- CSSxJS/main.js
+++ CSSxJS/main.js
@@ -1,3 +1,25 @@
-//Select steps two through four and then add the class "hide" to them.
-var futureStep = document.querySelectorAll("#stepTwo, #stepThree, #stepFour");
-futureStep.classList.add("hide");
+//When page loads, Select steps two through four and then add the class "hide" to them.
+window.onload = function hide() {
+    var list = document.querySelectorAll("li");
+    for (let i = 1; i < list.length; i++) {
+        list[i].classList.add("hide");
+    }
+}
+
+//When button is pressed it removes the hide class one at a time.
+if(! count) {
+    var count = 0;
+}
+function nextStep() {
+    let list = document.querySelectorAll("li");
+    count++;
+    if (count == 1) {
+        list[1].classList.remove("hide");
+    }
+    if (count == 2) {
+        list[2].classList.remove("hide");
+    }
+    if (count == 3) {
+        list[3].classList.remove("hide");
+    }
+}
