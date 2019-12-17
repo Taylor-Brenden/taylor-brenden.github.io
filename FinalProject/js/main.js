e5b3f0bbf0779d96a53025b3f1b672ac2ef61d77
Taylor-Brenden <btay46@gmail.com>
2019-12-16 22:40:00 -0700

    Update main.js

--- FinalProject/js/main.js
+++ FinalProject/js/main.js
@@ -1,4 +1,18 @@
+//overall DOM implementation
+const app = document.getElementById('root')
 
+const container = document.createElement('div')
+container.setAttribute('id', 'container')
+container.style.display = 'none'
+
+//When button is pressed
+function show() {
+  var x = document.getElementById("container")
+  x.style.display = "block"
+  app.classList.add("animation")
+};
+
+app.appendChild(container)
 // API implementation
 var request = new XMLHttpRequest()
 
@@ -14,17 +28,30 @@ request.onload = function() {
         //create div for each upcoming event
         const event = document.createElement('div')
         event.setAttribute('class', 'event')
+        event.ontouchstart = function press() {
+          event.classList.add("secondary");
+        }
+        event.ontouchend = function lift() {
+          event.classList.remove("secondary");
+        }
         //create an h1 for title of event
         const h1 = document.createElement('h1')
         h1.textContent = events.name.text
+        h1.setAttribute('class', 'title')
         // Create a p with event date
         const p = document.createElement('p')
         events.start.local = events.start.local.substring(0, 10) //cuts off unnecessary time info
         p.textContent = events.start.local
+        p.setAttribute('class', 'date')
+        //Add a summary
+        const sum = document.createElement('p')
+        sum.textContent = events.description.text
+        sum.setAttribute('class', 'summary')
         //Append the events to the container
         container.appendChild(event)
-        event.appendChild(h1)
         event.appendChild(p)
+        event.appendChild(h1)
+        event.appendChild(sum)
 
 
 
@@ -36,15 +63,3 @@ request.onload = function() {
 }
 
 request.send()
-
-//overall DOM implementation
-const app = document.getElementById('root')
-
-const logo = document.createElement('img')
-logo.src = '../img/BasementLogo.jpg'
-
-const container = document.createElement('div')
-container.setAttribute('class', 'container')
-
-app.appendChild(logo)
-app.appendChild(container)
